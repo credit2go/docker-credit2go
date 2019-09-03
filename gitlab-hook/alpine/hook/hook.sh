1e601e496b506ce5e9ebc748bc69d24aabfd72ac
jjlu@credit2go.cn <jjlu@credit2go.cn>
2019-09-03 18:38:00 +0800

    Change to always fetch jenkins template job config to update in case jenkins plugin upgrade.

--- gitlab-hook/alpine/hook/hook.sh
+++ gitlab-hook/alpine/hook/hook.sh
@@ -33,24 +33,28 @@ then
   echo "Jenkins Project Address is: ${job}"
   echo "Git Url is : ${gitrepo}"
   echo "branch name is : ${branch}"
-  #Check Return Code to choose step
-  #check job exists or not, if job not exists, then create Jenkins job automaticlly and trigger the first build
-  status=$(curl -s -I $job/ --user $user:$token|grep HTTP|awk {'print $2'})
-  if [ "$status" -ne 200 ]
-  then
-  	echo "Jenkins Job not configure"
-  	/bin/bash /opt/jenkins/createJenkinsJob.sh $job/ $branch $gitrepo
-  else
-  	#job exists
-  	#Jenkins Job API Call or Scan Branch
-  	url=$job/build?cause=$newrev
-  	status=$(curl -s -I -X POST $url --user $user:$token|grep HTTP|awk {'print $2'})
-  	if [ "$status" -eq 404 ]
-  	then
-  		url=$job/buildWithParameters?cause=${CI_COMMIT_SHA}
-  		curl -s -X POST $url --user $user:$token
-  	fi
-  fi
+  #Always update job from template even job exists
+  echo "Create or Update Jenkins Job"
+  /bin/bash /opt/jenkins/createJenkinsJob.sh $job/ $branch $gitrepo
+
+#  #Check Return Code to choose step
+#  #check job exists or not, if job not exists, then create Jenkins job automaticlly and trigger the first build
+#  status=$(curl -s -I $job/ --user $user:$token|grep HTTP|awk {'print $2'})
+#  if [ "$status" -ne 200 ]
+#  then
+#  	echo "Jenkins Job not configure"
+#  	/bin/bash /opt/jenkins/createJenkinsJob.sh $job/ $branch $gitrepo
+#  else
+#  	#job exists
+#  	#Jenkins Job API Call or Scan Branch
+#  	url=$job/build?cause=$newrev
+#  	status=$(curl -s -I -X POST $url --user $user:$token|grep HTTP|awk {'print $2'})
+#  	if [ "$status" -eq 404 ]
+#  	then
+#  		url=$job/buildWithParameters?cause=${CI_COMMIT_SHA}
+#  		curl -s -X POST $url --user $user:$token
+#  	fi
+#  fi
   echo "Jenkins Job has been notified"
 else
   echo "Jenkins is ignored as per its path: ${path} is not valid"
