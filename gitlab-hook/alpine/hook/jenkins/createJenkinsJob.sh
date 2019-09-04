aa218c9bb3922d2ff81e71d0fd58fcddd55bc508
jjlu@credit2go.cn <jjlu@credit2go.cn>
2019-09-04 09:44:53 +0800

    Change to always fetch jenkins template job config to update in case jenkins plugin upgrade.

--- gitlab-hook/alpine/hook/jenkins/createJenkinsJob.sh
+++ gitlab-hook/alpine/hook/jenkins/createJenkinsJob.sh
@@ -38,34 +38,53 @@ array=(${foldername//\// })
 url=$jenkins
 for var in ${array[@]}
 do
-		#创建Jenkins项目，类型Folder
+			cp /opt/jenkins/folder.xml /opt/jenkins/$var.xml
+			#修改descritpion
+			sed -i 's/Jenkins各种模版项目/''/g' /opt/jenkins/$var.xml
+			#修改displayname
+			sed -i 's/模版项目/'$var'/g' /opt/jenkins/$var.xml
+		#创建 Jenkins项目，类型Folder
 		url=$url/job/$var
+		createurl=${url%/job/$var}
 		status=$(curl -s -I -X POST $url/ --user $user:$token|grep HTTP|awk {'print $2'})
 		if [ "$status" -ne 200 ]
 		then
-			cp /opt/jenkins/folder.xml /opt/jenkins/$var.xml
-			#修改displayname
-			sed -i 's/模版项目/'$var'/g' /opt/jenkins/$var.xml
-			sed -i 's/Jenkins各种模版项目/''/g' /opt/jenkins/$var.xml
 			#Jenkins项目不存在，创建folder
-			createurl=${url%/job/$var}
+			echo "Create Folder"
 			curl -s -X POST $createurl'/createItem?name='$var --user $user:$token --data-binary @/opt/jenkins/$var.xml -H "Content-Type:text/xml"
+			rm -f /opt/jenkins/$var.xml
+		else
+			#Jenkins项目存在，Update folder
+			echo "Update Folder"
+			curl -s -X POST $url'/config.xml' --user $user:$token --data-binary @/opt/jenkins/$var.xml -H "Content-Type:text/xml"
 			rm -f /opt/jenkins/$var.xml
 		fi
 done
 #创建Jenkins 项目，类型MutliBranchPipeline
 cp /opt/jenkins/job.xml /opt/jenkins/$projectname.xml
+#修改description
+sed -i 's/即信GitLab的标准Gitflow模板项目//g' /opt/jenkins/$projectname.xml
 #修改displayname
 sed -i 's/多分支GitLab流水线/'$projectname'/g' /opt/jenkins/$projectname.xml
 #修改scm地址, normal Git plugin
 sed -i 's/'$gitlab'/'$giturl'/g' /opt/jenkins/$projectname.xml
-#修改scm地址, type GitLab Branch Source Plugin support
+#修改scm地址, GitLab Branch Source Plugin
 sed -i 's/template/'$foldername'/g' /opt/jenkins/$projectname.xml
 sed -i 's/gitflow/'$projectname'/g' /opt/jenkins/$projectname.xml
-createurl=${projecturl%/job/$projectname/}	
-curl -s -X POST $createurl'/createItem?name='$projectname --user $user:$token --data-binary @/opt/jenkins/$projectname.xml -H "Content-Type:text/xml"
+createurl=${projecturl%/job/$projectname/}
+status=$(curl -s -I projecturl/ --user $user:$token|grep HTTP|awk {'print $2'})
+if [ "$status" -ne 200 ]
+then
+    #
+    echo "Create Job"
+    curl -s -X POST $createurl'/createItem?name='$projectname --user $user:$token --data-binary @/opt/jenkins/$projectname.xml -H "Content-Type:text/xml"
+else
+    #
+    echo "Update Job"
+    curl -s -X POST ${projecturl}'/config.xml' --user $user:$token --data-binary @/opt/jenkins/$projectname.xml -H "Content-Type:text/xml"
+fi
 rm -rf /opt/jenkins/$projectname.xml
 echo "Jenkins Job has been created."
 #触发初始化构建
-curl -s -X POST $projecturl'/build?cause=initialize' --user $user:$token
+curl -s -X POST ${projecturl}'/build?cause=initialize' --user $user:$token
 exit 0
