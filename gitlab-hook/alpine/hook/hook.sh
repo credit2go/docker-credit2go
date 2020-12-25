6f68b1994bf54caf1d4905fed45b6198d3741fa5
jjlu@credit2go.cn <jjlu@credit2go.cn>
2020-12-25 11:29:46 +0800

    1. upgrade alpine from 3.10 to 3.11 2. add reportportal to gather trigger report to test report portal system

--- gitlab-hook/alpine/hook/hook.sh
+++ gitlab-hook/alpine/hook/hook.sh
@@ -3,7 +3,7 @@
 #this file trigger from GitLab-CI by using env defined in gitlab-runner
 
 #define variable from input or env
-robot -l NONE -r NONE -o NONE -v jenkins:${JENKINS_URL} -v user:${JENKINS_USER} -v password:${JENKINS_TOKEN} -v gitlab:${CI_API_V4_URL} -v token:${GITLAB_API_TOKEN} -v id:${CI_PROJECT_ID} /opt
+robot --listener robotframework_reportportal.listener --variable RP_UUID:"f41c5d6d-d04c-4c4a-9d7c-2acc8edc2b63" --variable RP_ENDPOINT:"https://reportportal.credit2go.cn" --variable RP_LAUNCH:"jenkins-gitlab-hook" --variable RP_PROJECT:"jenkins" -l NONE -r NONE -o NONE -v jenkins:${JENKINS_URL} -v user:${JENKINS_USER} -v password:${JENKINS_TOKEN} -v gitlab:${CI_API_V4_URL} -v token:${GITLAB_API_TOKEN} -v id:${CI_PROJECT_ID} /opt
 
 #Read job trigger status
 status=$(cat /opt/hook.txt)
